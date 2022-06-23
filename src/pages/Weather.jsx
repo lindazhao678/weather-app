f54b8fb170a265c0f6e0adc928a9c504e4939fab
lindazhao678 <lindazhao678@gmail.com>
2022-06-23 13:36:16 +1000

    spinner added

--- src/pages/Weather.jsx
+++ src/pages/Weather.jsx
@@ -4,11 +4,14 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Day from "../components/Day";
 import MyToDo from "../components/MyToDo";
-import axios from 'axios'
+import axios from 'axios';
+import Spinner from "react-bootstrap/Spinner";
 
 function Weather() {
   const [forecasts, setForecasts] = useState([]);
   const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(false);
+
   function handleTodos(current) {
     let myTodos = [];
     if (current.day.uv >= 5) {
@@ -26,19 +29,29 @@ function Weather() {
     setTodos(myTodos);
   }
   useEffect(() => {
+    setLoading(true);
     axios
       .get("https://melbourne-weather-app-api.herokuapp.com/")
       .then((response) => {
+        setLoading(false);
         setForecasts(response.data.forecast.forecastday);
         handleTodos(response.data.forecast.forecastday[0]);
       })
       .catch((err) => {
+        setLoading(false);
         console.log(err);
       });
   }, []);
 
   return (
     <Container>
+      {loading && (
+          <div className="text-center pt-5">
+            <Spinner animation="border" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </Spinner>
+          </div>
+        )}
       <Row>
         <MyToDo todos={todos} forecasts={forecasts} />
         {forecasts.map((forecast, index) => (
